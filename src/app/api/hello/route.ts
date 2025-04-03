8164d257a0362d71104e797fdc5d70705b07bd1a
qngu0461 <qngu0461@uni.sydney.edu.au>
2025-04-04 03:55:39 +1100

    Update content for header using Environment Variables

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
 export const runtime = "edge"; 
 
 export async function GET(request: Request) {
-  // @ts-expect-error
+  // @ts-expect-error - geo invalid
   const country = request.geo?.country || "your place";
 
   const countryNames: Record<string, string> = {
